6bcd722526a348d324dfe8c9b3de04b9dbd679c8
Leo Angelo Quigao <leoangelo@fbmsoftware.com>
2015-02-17 22:00:52 +0800

    Switched poco version to 1.4.6p4

--- Build-iOS-Framework.sh
+++ Build-iOS-Framework.sh
@@ -27,7 +27,7 @@ findlib () {
 NAME="Poco"
 INCLUDE_PATH="Includes"
 LIBRARY_PATH="Libraries"
-LIBRARY_NAMES="PocoFoundation PocoNet PocoJSON PocoData PocoDataSQLite PocoUtil"
+LIBRARY_NAMES="PocoFoundation PocoNet PocoJSON PocoData PocoDataSQLite"
 ARCHITECTURES="arm64 armv7 armv7s i686"
 FRAMEWORK="Frameworks/${NAME}.framework"
 
